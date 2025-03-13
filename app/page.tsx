97572a777fa9486c8dde80233105964e0f462e4d
Sunil Band <sunilband0007@gmail.com>
2025-03-13 15:47:00 +0530

    Refactor App component to integrate CodeEditor and ExportOptions, enhancing code editing and exporting capabilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,126 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import { usePreferencesStore } from "@/store/use-preferences-store";
+import { fonts } from "@/options";
+import { themes } from "@/options";
+import { cn } from "@/lib/utils";
+import CodeEditor from "@/components/CodeEditor";
+import WidthMeasurement from "@/components/WidthMeasurement";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Resizable } from "re-resizable";
+import ThemeSelect from "@/components/controls/ThemeSelect";
+import LanguageSelect from "@/components/controls/LanguageSelect";
+import { ResetIcon } from "@radix-ui/react-icons";
+import FontSelect from "@/components/controls/FontSelect";
+import FontSizeInput from "@/components/controls/FontSizeInput";
+import PaddingSlider from "@/components/controls/PaddingSlider";
+import BackgroundSwitch from "@/components/controls/BackgroundSwitch";
+import DarkModeSwitch from "@/components/controls/DarkModeSwitch";
+import ExportOptions from "@/components/controls/ExportOptions";
+
+function App() {
+  const [width, setWidth] = useState("auto");
+  const [showWidth, setShowWidth] = useState(false);
+
+  const theme = usePreferencesStore((state) => state.theme);
+  const padding = usePreferencesStore((state) => state.padding);
+  const fontStyle = usePreferencesStore((state) => state.fontStyle);
+  const showBackground = usePreferencesStore((state) => state.showBackground);
+
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    if (queryParams.size === 0) return;
+    const state = Object.fromEntries(queryParams);
+
+    usePreferencesStore.setState({
+      ...state,
+      code: state.code ? atob(state.code) : "",
+      autoDetectLanguage: state.autoDetectLanguage === "true",
+      darkMode: state.darkMode === "true",
+      fontSize: Number(state.fontSize || 18),
+      padding: Number(state.padding || 64),
+    });
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="dark min-h-screen flex flex-col gap-4 justify-center items-center bg-neutral-950 text-white p-4">
+      <link
+        rel="stylesheet"
+        href={themes[theme as keyof typeof themes].theme}
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="stylesheet"
+        href={fonts[fontStyle as keyof typeof fonts].src}
+        crossOrigin="anonymous"
+      />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="w-full overflow-auto flex grow items-center justify-center p-4 border rounded-xl border-b-gray-900">
+        <Resizable
+          enable={{ left: true, right: true }}
+          minWidth={padding * 2 + 300}
+          maxWidth="100%"
+          size={{ width }}
+          onResize={(e, dir, ref) => setWidth(ref.offsetWidth.toString())}
+          onResizeStart={() => setShowWidth(true)}
+          onResizeStop={() => setShowWidth(false)}
+        >
+          <div
+            className={cn(
+              "overflow-hidden mb-2 transition-all ease-out",
+              showBackground
+                ? themes[theme as keyof typeof themes].background
+                : "ring ring-neutral-900"
+            )}
+            style={{ padding }}
+            ref={editorRef}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            <CodeEditor />
+          </div>
+          <WidthMeasurement showWidth={showWidth} width={Number(width)} />
+          <div
+            className={cn(
+              "transition-opacity w-fit mx-auto -mt-4",
+              showWidth || width === "auto"
+                ? "invisible opacity-0 hidden"
+                : "visible opacity-100"
+            )}
+          >
+            <Button size="sm" onClick={() => setWidth("auto")} variant="ghost">
+              <ResetIcon className="mr-2" />
+              Reset width
+            </Button>
+          </div>
+        </Resizable>
+      </div>
+
+      <Card className="p-6 w-fit bg-neutral-900/90 backdrop-blur">
+        <CardContent className="flex flex-wrap gap-4 sm:gap-6 p-0">
+          <ThemeSelect />
+          <LanguageSelect />
+          <FontSelect />
+          <FontSizeInput />
+          <PaddingSlider />
+          <BackgroundSwitch />
+          <DarkModeSwitch />
+          <div className="w-px bg-neutral-800" />
+          <div className="place-self-center">
+            <ExportOptions
+              targetRef={
+                editorRef as unknown as React.RefObject<HTMLDivElement>
+              }
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
+
+export default App;
