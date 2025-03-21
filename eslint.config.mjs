70e5b880e3e6ab09b30fbc5c4f393af11ba2bab5
Sunil Band <sunilband0007@gmail.com>
2025-03-21 17:26:16 +0530

    Add ESLint configuration and ignore file for improved linting rules and file exclusions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,51 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:unicorn/recommended",
+    "plugin:import/recommended",
+    "plugin:tailwindcss/recommended",
+  ),
+  {
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      "tailwindcss/classnames-order": "error",
+      "tailwindcss/no-custom-classname": "off",
+      "unicorn/no-array-callback-reference": "off",
+      "unicorn/no-array-for-each": "off",
+      "unicorn/no-array-reduce": "off",
+      "unicorn/no-null": "off",
+      "unicorn/filename-case": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: {
+            e2e: true,
+          },
+          replacements: {
+            props: false,
+            ref: false,
+            params: false,
+          },
+        },
+      ],
+    },
+  },
+  {
+    plugins: ["simple-import-sort", "tailwindcss"],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
