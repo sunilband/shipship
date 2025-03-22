28be39fa6d50be935bfdd197a41f4ba2e52a9c00
Sunil Band <sunilband0007@gmail.com>
2025-03-22 17:29:58 +0530

    Enhance Next.js configuration with strict mode, ESLint settings, CSS optimization, and redirect for owner profile

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/owner",
+        destination: "https://github.com/aayushbharti",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
